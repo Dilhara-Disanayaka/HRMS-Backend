5aca301a7198490d28fe5c352191ac4b4df14601
Dilhara-Disanayaka <dilhara.22@cse.mrt.ac.lk>
2025-06-30 22:37:24 +0530

    add API end points

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // index.js
 const express = require('express');
 const app = express();
-
+import UserRouter from './Routes/UserRouter.jS';
 // Middleware to parse JSON
 app.use(express.json());
 
-// Simple route
+// api endpoints
+app.use('/api/user',UserRouter)
+app.use('/api/PIM',PIM)
+app.use('/api/AbsenceManagement',AbsentManagement)
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
