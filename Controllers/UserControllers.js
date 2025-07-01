6ebd20a7efd063582b674453e5c058a6be5eaf05
Dilhara-Disanayaka <dilhara.22@cse.mrt.ac.lk>
2025-07-01 23:10:59 +0530

    Add register controller

--- Controllers/UserControllers.js
+++ Controllers/UserControllers.js
@@ -8,3 +8,13 @@ const loginuser=async(req,res)=>{
           res.json({sucess:false,message:"invalid login"})
     }
 }
+const registeruser=async(req,res)=>{
+    try{
+        const{email,password}=req.body
+        const message=await registeruser(email,password)
+        res.json(message)
+    }catch(error){
+        res.json({sucess:false,masage:"fail registration"})
+    }
+}
+export default({loginuser,registeruser})
