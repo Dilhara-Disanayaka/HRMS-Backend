e4d3bc737e2881e86167e6c15c17dee5f547fac6
Dilhara-Disanayaka <dilhara.22@cse.mrt.ac.lk>
2025-07-18 23:38:56 +0530

    add mail sending function

---
+++ config/mailer.js
@@ -0,0 +1,10 @@
+const transporter = nodemailer.createTransport({
+    secure: true,
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASSWORD
+    }
+});
+module.exports = transporter;
